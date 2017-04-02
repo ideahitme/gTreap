b7dd55772d05d5e9d5d724d5d6478b2d0b2baf89
ideahitme <yerken.tussupbekov@zalando.de>
2017-04-03 01:23:22 +0200

    kickoff indexed treap

--- indexed.go
+++ indexed.go
@@ -1,24 +1,146 @@
 package treap
 
+import (
+	"math/rand"
+	"time"
+)
+
 // Indexed is
 type Indexed struct {
 	root *Node
-	cfg  *Config
 	size int
 }
 
-// Insert adds new element to the treap
-//
-func (it *Indexed) Insert(index, value int) {
-
+// NewIndexed returns new Indexed Tree
+func NewIndexed() *Indexed {
+	rand.Seed(time.Now().UTC().UnixNano())
+	return &Indexed{
+		size: 0,
+	}
 }
 
-// Find returns true if element exists in the treap
-func (it *Indexed) Find(x int) bool {
-	return false
+// Insert adds new element to the treap
+// index - index of the element - same as key in normal Tree
+// value - value of the element - normally something to be updated
+// if element with identical index is found, it is overwritten
+func (t *Indexed) Insert(index, value int) {
+	newNode := NewNodeWithValue(index, value)
+	l, r := t.split(t.root, index-1)
+	xl, xr := t.split(r, index)
+	if xl == nil {
+		t.size++
+	}
+	l = t.merge(l, newNode)
+	t.root = t.merge(l, xr)
+}
+
+// Slice returns slice by InOrder traversing
+// each element of the slice is a slice of form [index, value]
+// slice is sorted by index in increasing order
+func (t *Indexed) Slice() [][]int {
+	result := make([][]int, 0, t.size)
+	t.inOrder(t.root, func(cur *Node) {
+		result = append(result, []int{cur.key, cur.value})
+	})
+	return result
+}
+
+// Remove deletes node with key == x from the Indexed Treap
+func (t *Indexed) Remove(x int) {
+	l, r := t.split(t.root, x-1)
+	_, xr := t.split(r, x)
+	t.root = t.merge(l, xr)
+}
+
+// Find returns value of element in the treap with index == x, second parameter
+// indicates if element exists in the Treap
+func (t *Indexed) Find(x int) (value int, exists bool) {
+	n := t.find(t.root, x)
+	if n == nil {
+		return 0, false
+	}
+	return n.value, true
 }
 
 // Add adds "x" to all elements with index i <= t <= j
-func (it *Indexed) Add(x int, i, j int) {
+func (t *Indexed) Add(x int, i, j int) {
 
+}
+
+// Max returns maximum element (by value) with index i <= t <= j
+func (t *Indexed) Max(i, j int) int {
+	return -1
+}
+
+// Min returns minimum element (by value) with index i <= t <= j
+func (t *Indexed) Min(i, j int) int {
+	return -1
+}
+
+/** Private methods */
+
+// split recursively splits the treap with a given root into two Treaps l, r based on key
+// l - has all node keys less or equal to key
+// r - has all node keys strictly greater than key
+func (t *Indexed) split(root *Node, key int) (l *Node, r *Node) {
+	if root == nil {
+		return
+	}
+	if root.key > key {
+		subL, subR := t.split(root.left, key)
+		subR.SetParent(root)
+		root.left = subR
+		return subL, root
+	}
+	subL, subR := t.split(root.right, key)
+	subL.SetParent(root)
+	root.right = subL
+	return root, subR
+}
+
+// merge recursively merges two Treaps into one, maintaining Treap property
+// produces new root
+// it is required that every key in one of the Treaps is less than any key in another Treap
+func (t *Indexed) merge(l, r *Node) (root *Node) {
+	if l == nil {
+		return r
+	}
+	if r == nil {
+		return l
+	}
+	if l.key > r.key {
+		l, r = r, l
+	}
+	if l.p > r.p {
+		subR := t.merge(l.right, r)
+		subR.SetParent(l)
+		l.right = subR
+		return l
+	}
+	subL := t.merge(l, r.left)
+	subL.SetParent(r)
+	r.left = subL
+	return r
+}
+
+func (t *Indexed) inOrder(cur *Node, f func(cur *Node)) {
+	if cur == nil {
+		return
+	}
+	t.inOrder(cur.left, f)
+	f(cur)
+	t.inOrder(cur.right, f)
+}
+
+func (t *Indexed) find(cur *Node, x int) *Node {
+	if cur == nil {
+		return nil
+	}
+	if cur.key == x {
+		return cur
+	}
+	if cur.key > x {
+		return t.find(cur.left, x)
+	}
+	return t.find(cur.right, x)
 }
--- tree.go
+++ tree.go
@@ -24,28 +24,28 @@ func NewTree(cfg *Config) *Tree {
 }
 
 // Insert adds values to the Treap
-func (st *Tree) Insert(x ...int) {
+func (t *Tree) Insert(x ...int) {
 	for _, el := range x {
-		if !st.cfg.AllowDuplicates && st.find(st.root, el) {
+		if !t.cfg.AllowDuplicates && t.find(t.root, el) {
 			continue
 		}
 		newNode := NewNode(el)
-		l, r := st.split(st.root, el)
-		st.root = st.merge(l, newNode)
-		st.root = st.merge(st.root, r)
-		st.size++
+		l, r := t.split(t.root, el)
+		t.root = t.merge(l, newNode)
+		t.root = t.merge(t.root, r)
+		t.size++
 	}
 }
 
 // Find finds if value is present in the Treap
-func (st *Tree) Find(x int) bool {
-	return st.find(st.root, x)
+func (t *Tree) Find(x int) bool {
+	return t.find(t.root, x)
 }
 
 // Slice returns sorted slice by InOrder traversing
-func (st *Tree) Slice() []int {
-	result := make([]int, 0, st.size)
-	st.inOrder(st.root, func(cur *Node) {
+func (t *Tree) Slice() []int {
+	result := make([]int, 0, t.size)
+	t.inOrder(t.root, func(cur *Node) {
 		result = append(result, cur.key)
 	})
 	return result
@@ -53,21 +53,21 @@ func (st *Tree) Slice() []int {
 
 // Remove deletes node with key == x from the Treap
 // if multiple such elements exist only one is deleted
-func (st *Tree) Remove(x int) {
-	l, r := st.split(st.root, x-1)
-	xl, xr := st.split(r, x)
+func (t *Tree) Remove(x int) {
+	l, r := t.split(t.root, x-1)
+	xl, xr := t.split(r, x)
 	if xl != nil {
-		l = st.merge(l, xl.left)
-		xr = st.merge(xl.right, xr)
+		l = t.merge(l, xl.left)
+		xr = t.merge(xl.right, xr)
 	}
-	st.root = st.merge(l, xr)
+	t.root = t.merge(l, xr)
 }
 
 // RemoveAll deletes all node with key == x from the Treap
-func (st *Tree) RemoveAll(x int) {
-	l, r := st.split(st.root, x-1)
-	_, xr := st.split(r, x)
-	st.root = st.merge(l, xr)
+func (t *Tree) RemoveAll(x int) {
+	l, r := t.split(t.root, x-1)
+	_, xr := t.split(r, x)
+	t.root = t.merge(l, xr)
 }
 
 /** Private methods */
@@ -75,17 +75,17 @@ func (st *Tree) RemoveAll(x int) {
 // split recursively splits the treap with a given root into two Treaps l, r based on key
 // l - has all node keys less or equal to key
 // r - has all node keys strictly greater than key
-func (st *Tree) split(root *Node, key int) (l *Node, r *Node) {
+func (t *Tree) split(root *Node, key int) (l *Node, r *Node) {
 	if root == nil {
 		return
 	}
 	if root.key > key {
-		subL, subR := st.split(root.left, key)
+		subL, subR := t.split(root.left, key)
 		subR.SetParent(root)
 		root.left = subR
 		return subL, root
 	}
-	subL, subR := st.split(root.right, key)
+	subL, subR := t.split(root.right, key)
 	subL.SetParent(root)
 	root.right = subL
 	return root, subR
@@ -94,7 +94,7 @@ func (st *Tree) split(root *Node, key int) (l *Node, r *Node) {
 // merge recursively merges two Treaps into one, maintaining Treap property
 // produces new root
 // it is required that every key in one of the Treaps is less than any key in another Treap
-func (st *Tree) merge(l, r *Node) (root *Node) {
+func (t *Tree) merge(l, r *Node) (root *Node) {
 	if l == nil {
 		return r
 	}
@@ -105,27 +105,27 @@ func (st *Tree) merge(l, r *Node) (root *Node) {
 		l, r = r, l
 	}
 	if l.p > r.p {
-		subR := st.merge(l.right, r)
+		subR := t.merge(l.right, r)
 		subR.SetParent(l)
 		l.right = subR
 		return l
 	}
-	subL := st.merge(l, r.left)
+	subL := t.merge(l, r.left)
 	subL.SetParent(r)
 	r.left = subL
 	return r
 }
 
-func (st *Tree) inOrder(cur *Node, f func(cur *Node)) {
+func (t *Tree) inOrder(cur *Node, f func(cur *Node)) {
 	if cur == nil {
 		return
 	}
-	st.inOrder(cur.left, f)
+	t.inOrder(cur.left, f)
 	f(cur)
-	st.inOrder(cur.right, f)
+	t.inOrder(cur.right, f)
 }
 
-func (st *Tree) find(cur *Node, x int) bool {
+func (t *Tree) find(cur *Node, x int) bool {
 	if cur == nil {
 		return false
 	}
@@ -133,7 +133,7 @@ func (st *Tree) find(cur *Node, x int) bool {
 		return true
 	}
 	if cur.key > x {
-		return st.find(cur.left, x)
+		return t.find(cur.left, x)
 	}
-	return st.find(cur.right, x)
+	return t.find(cur.right, x)
 }
